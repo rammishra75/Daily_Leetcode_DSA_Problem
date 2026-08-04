4185c75a76d0cf1208932be5845c2c09b3b70654
Mishra Ram Pankaj <mishraram7524@gmail.com>
2026-08-04 21:46:18 +0530

    Time: 2 ms (88.56%) | Memory: 47.9 MB (43.41%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJump = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(i > maxJump) return false;
+            maxJump = Math.max(maxJump, i + nums[i]);
+        }
+        return true;
+    }
+}
