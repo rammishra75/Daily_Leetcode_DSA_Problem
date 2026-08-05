f0d8a688d574f9c6befa33bed74970198b6c1468
Mishra Ram Pankaj <mishraram7524@gmail.com>
2026-08-05 21:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.63%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int func(int ind, int[] nums, int[] dp){
+        if(ind == 0) return nums[ind];
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums[ind] + func(ind - 2, nums, dp);
+        int notPick = 0 + func(ind - 1, nums, dp);
+        return dp[ind] = Math.max(pick, notPick);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return func(n - 1, nums, dp);
+    }
+}
