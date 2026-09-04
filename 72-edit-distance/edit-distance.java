0a70d8411883bae8721d9030e3e58ac32d4f0410
Mishra Ram Pankaj <mishraram7524@gmail.com>
2026-09-04 14:53:47 +0530

    Time: 4 ms (75.50%) | Memory: 46.2 MB (94.87%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,16 +1,24 @@
 class Solution {
-    public int func(int i, int j, String s1, String s2, int[][] dp){
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = func(i - 1, j - 1, s1, s2, dp);
-        return dp[i][j] = 1 + Math.min(func(i - 1, j, s1, s2, dp), Math.min(func(i, j - 1, s1, s2, dp), func(i - 1, j - 1, s1, s2, dp)));
-    }
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return func(n - 1, m - 1, word1, word2, dp);
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
+        for(int j = 0; j <= m; j++){
+            prev[j] = j;
+        }
+        for(int i = 1; i <= n; i++){
+            curr[0] = i;
+            for(int j = 1; j <= m; j++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    curr[j] = prev[j - 1];
+                }
+                else{
+                    curr[j] =  1 + Math.min(curr[j - 1] , Math.min(prev[j - 1], prev[j]));
+                }
+            }
+            prev = curr.clone();
+        }
+        return prev[m];
     }
 }
